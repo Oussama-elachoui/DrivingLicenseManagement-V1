8bb68a4e2b85f545abc4172e3131b853fb4f35a1
Oussama-elachoui <oussama.elachoui2@gmail.com>
2025-02-18 20:18:12 +0100

    ADD CLS-Counries ADD Sql-Countries

---
+++ DATA-TIER/SQL-Countries.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA_TIER
+{
+    public class SQL_Countries
+    {
+        public static DataTable GetTable()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(StringConnection.connectionString);
+            string query = "SELECT * FROM Countries";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+            return dt;
+
+        }
+        public static bool GetCountryByID(int CountryID, ref string CountryName)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(StringConnection.connectionString);
+            string query = "SELECT * FROM Countries WHERE CountryID = @CountryID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CountryID", CountryID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    CountryName = (string)reader["CountryName"];
+                    isFound = true;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                isFound = false;
+
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+            return isFound;
+
+        }
+        public static bool GetCountryByName(string CountryName, ref int CountryID )
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(StringConnection.connectionString);
+            string query = "SELECT * FROM Countries WHERE CountryName = @CountryName";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CountryName", CountryName);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    CountryID = (int)reader["CountryID"];
+                    isFound = true;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                isFound = false;
+
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+            return isFound;
+
+        }
+
+
+
+    }
+}
---
+++ Logic-TIER/Cls-Countries.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_TIER
+{
+    public class Cls_Countries
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        public Cls_Countries() 
+        {
+            CountryID=-1;
+            CountryName = "";
+        }
+
+        public Cls_Countries(int CountryID, string CountryName)
+        {
+            this.CountryID = CountryID;
+            this.CountryName = CountryName;
+        }
+
+        public static Cls_Countries FindById(int CountryID)
+        {
+            string CountryName = "";
+
+            if (DATA_TIER.SQL_Countries.GetCountryByID(CountryID, ref CountryName))
+            {
+                return new Cls_Countries(CountryID, CountryName);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        public static Cls_Countries FindByCountryName(string CountryName)
+        {
+            int CountryID = -1;
+
+            if (DATA_TIER.SQL_Countries.GetCountryByName(CountryName, ref CountryID))
+            {
+                return new Cls_Countries(CountryID, CountryName);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+
+    }
+}
